46ee49452ab65f867cef58c3d9a28cfb709c515b
Conrad Christensen <conrad.christensen@gmail.com>
2017-07-06 09:42:34 +0200

    Initial commit

---
+++ src/main/java/io/smartmachine/api/ServiceOneApplication.java
@@ -0,0 +1,12 @@
+package io.smartmachine.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOneApplication.class, args);
+	}
+}
